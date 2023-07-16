5143dfdec72a2f2ff0d3d96a546ed2ba14de73a9
Kobe <kobetbuckley@gmail.com>
2023-07-15 15:53:34 -1000

    initial setup of the redux example using posts as this is relevant in structure to ideas in mind for application

--- val-app/src/app/store.ts
+++ val-app/src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit"
 import counterReducer from "../features/counter/counterSlice"
+import postsReducer from "../features/posts/postsSlice.js"
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    posts: postsReducer
   },
 })
 
