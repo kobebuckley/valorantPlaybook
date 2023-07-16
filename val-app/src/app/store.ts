5e4a4cbc9e81ed461477b9972447a40d04660deb
Kobe <kobetbuckley@gmail.com>
2023-07-15 16:50:29 -1000

    slight reset to previous code

--- val-app/src/app/store.ts
+++ val-app/src/app/store.ts
@@ -1,6 +1,6 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit"
 import counterReducer from "../features/counter/counterSlice"
-import postsReducer from "../features/posts/postsSlice.js"
+import postsReducer from "../features/posts/postsSlice"
 
 export const store = configureStore({
   reducer: {
