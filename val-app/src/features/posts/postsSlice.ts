bf8e8b9fa05c5e04c56bb65414a2c94d3cc94638
Kobe <kobetbuckley@gmail.com>
2023-07-20 11:54:19 -1000

    updated the postSlicer the reducer for editing posts

--- val-app/src/features/posts/postsSlice.ts
+++ val-app/src/features/posts/postsSlice.ts
@@ -35,6 +35,15 @@ const postsSlice = createSlice({
   reducers: {
     postAdded(state, action) {
       state.push(action.payload)
+    },
+    postUpdated(state, action) {
+      const { id, title, content, videoUrl } = action.payload
+      const existingPost = state.find(post => post.id === id)
+      if (existingPost) {
+        existingPost.title = title
+        existingPost.content = content
+        existingPost.videoUrl = videoUrl
+      }
     }
   }
 });
