3d6da2ad915a0a290140fb4dc7b8f5db6a6fda8e
Kobe <kobetbuckley@gmail.com>
2023-07-22 11:23:26 -1000

    moving on; can include uploading images api and that stuff later on

--- val-app/src/features/posts/EditPostForm.tsx
+++ val-app/src/features/posts/EditPostForm.tsx
@@ -13,7 +13,7 @@ export const EditPostForm: React.FC = () => {
 
   const [title, setTitle] = useState(post?.title || '');
   const [content, setContent] = useState(post?.content || '');
-  const [videoUrl, setVideoUrl] = useState(post?.videoUrl || ''); // Initialize with an empty string
+  const [videoUrl, setVideoUrl] = useState(post?.videoUrl || '');
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -37,37 +37,58 @@ export const EditPostForm: React.FC = () => {
 
   return (
     <section>
-      <h2>Edit Post</h2>
-      <form>
-        <label htmlFor="postTitle">Post Title:</label>
-        <input
-          type="text"
-          id="postTitle"
-          name="postTitle"
-          placeholder="What's on your mind?"
-          value={title}
-          onChange={onTitleChanged}
-        />
-        <label htmlFor="postContent">Content:</label>
-        <textarea
-          id="postContent"
-          name="postContent"
-          value={content}
-          onChange={onContentChanged}
-        />
-        <label htmlFor="videoUrlContent">Video:</label>
-        <input
-          type="text"
-          id="videoUrlContent"
-          name="videoUrlContent"
-          value={videoUrl}
-          onChange={onVideoUrlChanged}
-        />
+      <h2 className="text-3xl font-bold mb-4">Edit Post</h2>
+      <form className="flex flex-col gap-4">
+        <div className="flex flex-col gap-1">
+          <label htmlFor="postTitle" className="text-lg">Post Title:</label>
+          <input
+            type="text"
+            id="postTitle"
+            name="postTitle"
+            placeholder="What's on your mind?"
+            value={title}
+            onChange={onTitleChanged}
+            className="border border-gray-300 rounded-md p-2"
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <label htmlFor="postContent" className="text-lg">Content:</label>
+          <textarea
+            id="postContent"
+            name="postContent"
+            value={content}
+            onChange={onContentChanged}
+            className="border border-gray-300 rounded-md p-2"
+            rows={6}
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <label htmlFor="videoUrlContent" className="text-lg">Video:</label>
+          <input
+            type="text"
+            id="videoUrlContent"
+            name="videoUrlContent"
+            value={videoUrl}
+            onChange={onVideoUrlChanged}
+            className="border border-gray-300 rounded-md p-2"
+          />
+        </div>
       </form>
-      <button type="button" onClick={onSavePostClicked}>
-        Save Post
-      </button>
-      <Link to={`/posts/${postId}`}>Cancel</Link>
+      <div className="mt-4">
+        <button
+          type="button"
+          onClick={onSavePostClicked}
+          className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+        >
+          Save Post
+        </button>
+        <Link
+          to={`/posts/${postId}`}
+          className="px-4 py-2 ml-2 text-blue-500 hover:text-blue-700"
+        >
+          Cancel
+        </Link>
+      </div>
     </section>
   );
 };
