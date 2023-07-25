2bccee2cbbd367e0488e88bc1d3d04e0805a675a
Kobe <kobetbuckley@gmail.com>
2023-07-25 05:02:13 -1000

    added userId as the author param for postsSlice

--- val-app/src/features/posts/postsSlice.ts
+++ val-app/src/features/posts/postsSlice.ts
@@ -6,6 +6,7 @@ interface Post {
   content: string;
   videoUrl: string;
   agent: string;
+  userId: string;
 }
 
 const initialState: Post[] = [
@@ -14,42 +15,52 @@ const initialState: Post[] = [
     title: 'Liquid Nats!',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu dapibus libero. Nulla dapibus, pursdfsqafdfsdfsfd',
     videoUrl: 'https://www.youtube.com/watch?v=0t-A41qBGmg&pp=ygUKZ2Vra28gcHJvIA%3D%3D',
-    agent: 'gekko'
+    agent: 'gekko',
+    userId: '0'
   },
   {
     id: '2',
     title: 'Second Post',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu dapibus libero. Nulla dapibus, pursdfsqafdfsdfsfd',
     videoUrl: 'https://www.youtube.com/watch?v=7lVE9BQENGg',
-    agent: 'gekko'
+    agent: 'gekko',
+    userId: '0'
+
   },
   {
     id: '3',
     title: 'Third Post!',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu dapibus libero. Nulla dapibus, pursdfsqafdfsdfsfd',
     videoUrl: 'https://www.youtube.com/watch?v=ohu59Ssdq7g',
-    agent: 'gekko'
+    agent: 'gekko',
+    userId: '1'
+
   },
   {
     id: '4',
     title: 'Liquid Nats Again!',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu dapibus libero. Nulla dapibus, pursdfsqafdfsdfsfd',
     videoUrl: 'https://www.youtube.com/watch?v=aVgkN9dMXCY&pp=ygUKZ2Vra28gcHJvIA%3D%3D',
-    agent: 'gekko'
+    agent: 'gekko',
+    userId: '2'
   },
   {
     id: '5',
     title: 'Sen Sick!',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu dapibus libero. Nulla dapibus, pursdfsqafdfsdfsfd',
     videoUrl: 'https://www.youtube.com/watch?v=i7OsvSE4MEU&pp=ygUJZmFkZSBwcm8g',
-    agent: 'fade'
+    agent: 'fade',
+    userId: '2'
+
   },
   {
     id: '6',
     title: '100T Asuna',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu dapibus libero. Nulla dapibus, pursdfsqafdfsdfsfd',
     videoUrl: 'https://www.youtube.com/watch?v=AptQ7AXVXmw&pp=ygUSZmFkZSB2YWxvcmFudCBwcm8g',
-    agent: 'fade'
+    agent: 'fade',
+    userId: '2'
+
   }
 ];
 
@@ -61,14 +72,15 @@ const postsSlice = createSlice({
       reducer(state, action: PayloadAction<Post>) {
         state.push(action.payload);
       },
-      prepare(title: string, content: string, videoUrl: string, agent: string): { payload: Post } {
+      prepare(title: string, content: string, videoUrl: string, agent: string, userId: string): { payload: Post } {
         return {
           payload: {
             id: nanoid(),
             title,
             content,
             videoUrl,
-            agent
+            agent,
+            userId
           },
         };
       },
