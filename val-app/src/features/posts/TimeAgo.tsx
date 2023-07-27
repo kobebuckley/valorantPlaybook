f7c1e6cdfa60a7a871d358e21b776f5c6df8a47f
Kobe <kobetbuckley@gmail.com>
2023-07-27 09:53:24 -1000

    added the timeago component to handle date and time while inserting wherever desired

---
+++ val-app/src/features/posts/TimeAgo.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { parseISO, formatDistanceToNow } from 'date-fns';
+
+interface TimeAgoProps {
+  timestamp: string; 
+}
+
+export const TimeAgo: React.FC<TimeAgoProps> = ({ timestamp }) => {
+  let timeAgo = '';
+  if (timestamp) {
+    const date = parseISO(timestamp);
+    const timePeriod = formatDistanceToNow(date);
+    timeAgo = `${timePeriod} ago`;
+  }
+
+  return (
+    <span title={timestamp}>
+      &nbsp; <i>{timeAgo}</i>
+    </span>
+  );
+};
