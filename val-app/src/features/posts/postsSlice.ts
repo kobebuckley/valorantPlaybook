a2a77dfaeed2e5ef6d1c122b9d5b032af2ef14a8
Kobe <kobetbuckley@gmail.com>
2023-07-27 09:39:17 -1000

    date timestamp added to postSlice

--- val-app/src/features/posts/postsSlice.ts
+++ val-app/src/features/posts/postsSlice.ts
@@ -2,6 +2,7 @@ import { PayloadAction, createSlice, nanoid } from '@reduxjs/toolkit';
 
 interface Post {
   id: string;
+  date: string;
   title: string;
   content: string;
   videoUrl: string;
@@ -12,6 +13,7 @@ interface Post {
 const initialState: Post[] = [
   {
     id: '1',
+    date:'',
     title: 'Liquid Nats!',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu dapibus libero. Nulla dapibus, pursdfsqafdfsdfsfd',
     videoUrl: 'https://www.youtube.com/watch?v=0t-A41qBGmg&pp=ygUKZ2Vra28gcHJvIA%3D%3D',
@@ -20,6 +22,7 @@ const initialState: Post[] = [
   },
   {
     id: '2',
+    date:'',
     title: 'Second Post',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu dapibus libero. Nulla dapibus, pursdfsqafdfsdfsfd',
     videoUrl: 'https://www.youtube.com/watch?v=7lVE9BQENGg',
@@ -29,6 +32,7 @@ const initialState: Post[] = [
   },
   {
     id: '3',
+    date:'',
     title: 'Third Post!',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu dapibus libero. Nulla dapibus, pursdfsqafdfsdfsfd',
     videoUrl: 'https://www.youtube.com/watch?v=ohu59Ssdq7g',
@@ -38,6 +42,7 @@ const initialState: Post[] = [
   },
   {
     id: '4',
+    date:'',
     title: 'Liquid Nats Again!',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu dapibus libero. Nulla dapibus, pursdfsqafdfsdfsfd',
     videoUrl: 'https://www.youtube.com/watch?v=aVgkN9dMXCY&pp=ygUKZ2Vra28gcHJvIA%3D%3D',
@@ -46,6 +51,7 @@ const initialState: Post[] = [
   },
   {
     id: '5',
+    date:'',
     title: 'Sen Sick!',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu dapibus libero. Nulla dapibus, pursdfsqafdfsdfsfd',
     videoUrl: 'https://www.youtube.com/watch?v=i7OsvSE4MEU&pp=ygUJZmFkZSBwcm8g',
@@ -55,6 +61,7 @@ const initialState: Post[] = [
   },
   {
     id: '6',
+    date:'',
     title: '100T Asuna',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu dapibus libero. Nulla dapibus, pursdfsqafdfsdfsfd',
     videoUrl: 'https://www.youtube.com/watch?v=AptQ7AXVXmw&pp=ygUSZmFkZSB2YWxvcmFudCBwcm8g',
@@ -72,10 +79,11 @@ const postsSlice = createSlice({
       reducer(state, action: PayloadAction<Post>) {
         state.push(action.payload);
       },
-      prepare(title: string, content: string, videoUrl: string, agent: string, userId: string): { payload: Post } {
+      prepare(title: string, date: string, content: string, videoUrl: string, agent: string, userId: string): { payload: Post } {
         return {
           payload: {
             id: nanoid(),
+            date: new Date().toISOString(),
             title,
             content,
             videoUrl,
@@ -86,10 +94,11 @@ const postsSlice = createSlice({
       },
     },
     postUpdated(state, action: PayloadAction<Post>) {
-      const { id, title, content, videoUrl, agent } = action.payload;
+      const { id, date, title, content, videoUrl, agent } = action.payload;
       const existingPost = state.find((post) => post.id === id);
       if (existingPost) {
         existingPost.title = title;
+        existingPost.date = date;
         existingPost.content = content;
         existingPost.videoUrl = videoUrl;
         existingPost.agent = agent;
