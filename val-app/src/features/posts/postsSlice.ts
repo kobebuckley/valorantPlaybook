e456bb1028808a3291c82d2c7e405406c9691974
Kobe <kobetbuckley@gmail.com>
2023-07-27 09:59:47 -1000

    inserted date and time placeholders for postslice initial state

--- val-app/src/features/posts/postsSlice.ts
+++ val-app/src/features/posts/postsSlice.ts
@@ -1,4 +1,6 @@
 import { PayloadAction, createSlice, nanoid } from '@reduxjs/toolkit';
+import { sub } from 'date-fns'
+
 
 interface Post {
   id: string;
@@ -13,7 +15,7 @@ interface Post {
 const initialState: Post[] = [
   {
     id: '1',
-    date:'',
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
     title: 'Liquid Nats!',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu dapibus libero. Nulla dapibus, pursdfsqafdfsdfsfd',
     videoUrl: 'https://www.youtube.com/watch?v=0t-A41qBGmg&pp=ygUKZ2Vra28gcHJvIA%3D%3D',
@@ -22,7 +24,7 @@ const initialState: Post[] = [
   },
   {
     id: '2',
-    date:'',
+    date: sub(new Date(), { minutes: 20 }).toISOString(),
     title: 'Second Post',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu dapibus libero. Nulla dapibus, pursdfsqafdfsdfsfd',
     videoUrl: 'https://www.youtube.com/watch?v=7lVE9BQENGg',
@@ -42,7 +44,7 @@ const initialState: Post[] = [
   },
   {
     id: '4',
-    date:'',
+    date: sub(new Date(), { minutes: 2 }).toISOString(),
     title: 'Liquid Nats Again!',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu dapibus libero. Nulla dapibus, pursdfsqafdfsdfsfd',
     videoUrl: 'https://www.youtube.com/watch?v=aVgkN9dMXCY&pp=ygUKZ2Vra28gcHJvIA%3D%3D',
@@ -51,7 +53,7 @@ const initialState: Post[] = [
   },
   {
     id: '5',
-    date:'',
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
     title: 'Sen Sick!',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu dapibus libero. Nulla dapibus, pursdfsqafdfsdfsfd',
     videoUrl: 'https://www.youtube.com/watch?v=i7OsvSE4MEU&pp=ygUJZmFkZSBwcm8g',
@@ -61,7 +63,7 @@ const initialState: Post[] = [
   },
   {
     id: '6',
-    date:'',
+    date: sub(new Date(), { minutes: 45 }).toISOString(),
     title: '100T Asuna',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu dapibus libero. Nulla dapibus, pursdfsqafdfsdfsfd',
     videoUrl: 'https://www.youtube.com/watch?v=AptQ7AXVXmw&pp=ygUSZmFkZSB2YWxvcmFudCBwcm8g',
