ef2013ce29048775fe562026c0fd1faa4797e45d
Kobe <kobetbuckley@gmail.com>
2023-08-12 19:06:21 -1000

    the general add post features are working to upload the data to the database. Will make sure other posts features work and then clean up to tie back in certain features as possible

--- val-app/src/features/posts/AddPostForm.tsx
+++ val-app/src/features/posts/AddPostForm.tsx
@@ -1,270 +1,158 @@
-import React, { useState, useEffect } from "react";
-import { addDoc, collection } from "firebase/firestore";
-import { db, auth } from "../../firebase/firebase-config";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect, useContext, ChangeEvent, FormEvent } from 'react';
+import { addDoc, collection } from 'firebase/firestore';
+import { db, auth } from '../../firebase/firebase-config';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../context/auth-context';
+import ErrorModal from './ErrorModal';
 
-function AddPostForm({ isAuth }: { isAuth: boolean }) {
-  const [title, setTitle] = useState("");
-  const [postText, setPostText] = useState("");
-  const postsCollectionRef = collection(db, "posts");
+function AddPostForm() {
+  const [title, setTitle] = useState('');
+  const [postText, setPostText] = useState('');
+  const [videoUrl, setVideoUrl] = useState('');
+  const [agent, setAgent] = useState('');
+  const [showErrorModal, setShowErrorModal] = useState(false);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const postsCollectionRef = collection(db, 'posts');
   const navigate = useNavigate();
-  const createPost = async () => {
-    if (!auth.currentUser) {
-      return; // Exit if user is not authenticated
-    }
-  
-    try {
-      await addDoc(postsCollectionRef, {
-        title,
-        postText,
-        author: { name: auth.currentUser.displayName, id: auth.currentUser.uid },
-      });
-  
-      // Clear input fields after successful submission
-      setTitle("");
-      setPostText("");
-  
-      navigate("/");
-    } catch (error) {
-      console.error("Error creating post:", error);
-      // Handle error (e.g., show a user-friendly message)
-    }
-  };
-  
+  const { currentUser } = useContext(AuthContext);
+  const displayName = currentUser?.displayName || '';
 
   useEffect(() => {
-    if (!isAuth) {
-      navigate("/login");
+    if (!currentUser) {
+      navigate('/login');
     }
   }, []);
 
+  const onAgentChanged = (e: ChangeEvent<HTMLSelectElement>) => setAgent(e.target.value);
+  const onTitleChanged = (e: ChangeEvent<HTMLInputElement>) => setTitle(e.target.value);
+
+  const createPost = async () => {
+    if (!auth.currentUser) {
+      return;
+    }
+
+    try {
+      await addDoc(postsCollectionRef, {
+        date: new Date(),
+        title,
+        content: postText,
+        videoUrl,
+        agent,
+        userId: auth.currentUser.uid, // Use Firebase UID
+        reactions: {
+          thumbsUp: 0,
+          hooray: 0,
+          heart: 0,
+          rocket: 0,
+          eyes: 0,
+        },
+        moderated: false,
+      });
+
+      setTitle('');
+      setPostText('');
+      setVideoUrl('');
+      setAgent('');
+      setShowSuccessMessage(true);
+
+      navigate('/');
+    } catch (error) {
+      console.error('Error creating post:', error);
+    }
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (!currentUser) {
+      setShowErrorModal(true);
+      return;
+    }
+
+    createPost();
+  };
+
+  const canSave = Boolean(title) && Boolean(postText) && Boolean(videoUrl) && Boolean(agent);
+  const isSaveButtonDisabled = !canSave;
+
   return (
-    <div className="createPostPage">
-      <div className="cpContainer">
-        <h1>Create A Post</h1>
-        <div className="inputGp">
-          <label> Title:</label>
-          <input
-            placeholder="Title..."
-            value={title}
-            onChange={(event) => {
-              setTitle(event.target.value);
-            }}
-          />
+    <section className="p-4 bg-gray-900 text-white rounded shadow-lg">
+      <h2 className="text-2xl font-bold mb-4">Share Your Gaming Experience</h2>
+      {!auth.currentUser && (
+        <ErrorModal onClose={() => setShowErrorModal(false)} message={''} />
+      )}
+
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label htmlFor="agentSelect" className="block font-semibold mb-2 text-xl">
+            Select Agent:
+          </label>
+          <select
+            id="agentSelect"
+            name="agentSelect"
+            value={agent}
+            onChange={onAgentChanged}
+            className="border border-gray-800 rounded p-2 w-full bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          >
+            <option value="">Select an agent</option>
+            <option value="gekko">Gekko</option>
+            <option value="fade">Fade</option>
+          </select>
         </div>
-        <div className="inputGp">
-          <label> Post:</label>
-          <textarea
-            placeholder="Post..."
-            value={postText}
-            onChange={(event) => {
-              setPostText(event.target.value);
-            }}
-          />
-        </div>
-        <button onClick={createPost}>Submit Post</button>
-      </div>
-    </div>
+
+        <label htmlFor="postTitle" className="block font-semibold mb-2 text-xl">
+          Game Title:
+        </label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          value={title}
+          onChange={onTitleChanged}
+          className="border border-gray-800 rounded p-2 mb-4 w-full bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          placeholder="Enter the game title"
+        />
+
+        <label htmlFor="postContent" className="block font-semibold mb-2 text-xl">
+          Walkthrough Details:
+        </label>
+        <textarea
+          id="postContent"
+          name="postContent"
+          value={postText}
+          onChange={(e) => setPostText(e.target.value)}
+          className="border border-gray-800 rounded p-4 mb-4 w-full h-48 bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          placeholder="Share your in-depth walkthrough here..."
+        />
+
+<label htmlFor="postVideo" className="block font-semibold mb-2 text-xl">
+  Video URL:
+</label>
+<input
+  type="text"
+  id="postVideo"
+  name="postVideo"
+  value={videoUrl}
+  onChange={(e) => setVideoUrl(e.target.value)}
+  className="border border-gray-800 rounded p-2 mb-4 w-full bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
+  placeholder="Paste video URL here"
+/>
+
+<button
+  type="submit"
+  disabled={isSaveButtonDisabled}
+  className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+>
+  Save Post
+</button>
+      </form>
+      {showSuccessMessage && (
+        <p className="text-green-500 mt-2">
+          Post submitted for approval!
+        </p>
+      )}
+    </section>
   );
 }
 
 export default AddPostForm;
-
-/*
-Updated version here
-*/
-
-// import { nanoid } from '@reduxjs/toolkit';
-// import { FormEvent } from 'react';
-// import React, { useState, useEffect, ChangeEvent } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { Link, useNavigate, useParams } from 'react-router-dom';
-// import { RootState } from '../../app/store';
-// import { Post, postAdded, postUpdated, selectPostById } from './postsSlice';
-// import { User, selectLoggedInUser, setLoggedInUser } from '../users/usersSlice'; 
-// import ErrorModal from './ErrorModal';
-
-
-
-// /*
-// Updated version here
-// */
-
-
-
-
-// /*
-// Updated version here
-// */
-
-// export const AddPostForm: React.FC = () => {
-//   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-
-
-  
-//       const [title, setTitle] = useState<string>('');
-//       const [content, setContent] = useState<string>('');
-//       const [videoUrl, setVideoUrl] = useState<string>('');
-//       const [agent, setAgent] = useState<string>(''); 
-//       const [userId, setUserId] = useState<string>('');
-//       const [showErrorModal, setShowErrorModal] = useState(false); 
-
-      
-//   const dispatch = useDispatch();
-//   const loggedInUser = useSelector(selectLoggedInUser); 
-
-//   const onTitleChanged = (e: ChangeEvent<HTMLInputElement>) => setTitle(e.target.value);
-//   const onContentChanged = (e: ChangeEvent<HTMLTextAreaElement>) => setContent(e.target.value);
-//   const onVideoUrlChanged = (e: ChangeEvent<HTMLInputElement>) => setVideoUrl(e.target.value);
-//   const onAgentChanged = (e: ChangeEvent<HTMLSelectElement>) => setAgent(e.target.value);
-//   const onAuthorChanged = (e: ChangeEvent<HTMLSelectElement>) => setUserId(e.target.value)
-  
-  
-//   const onSavePostClicked = () => {
-
-//     if (title && content && videoUrl && agent && userId) {
-//      const newPost: Post = {
-//     id: nanoid(),
-//     title,
-//     content,
-//     videoUrl,
-//     date: new Date().toISOString(),
-//     agent,
-//     userId: loggedInUser!.id, // Use the logged-in user's id
-//     reactions: {},
-//     status: 'pending', 
-//     moderated: false
-//       };
-
-//       dispatch(postAdded(title, content, videoUrl, agent, userId));
-
- 
-//       setShowSuccessMessage(true);
-//       setTimeout(() => {
-//         setShowSuccessMessage(false);
-//         setTitle('');
-//         setContent('');
-//         setVideoUrl('');
-//         setAgent('');
-//         setUserId('');
-//       }, 3000); // Clear form after 3 seconds
-//     }
-//   };
-
-
-//   console.log('loggedInUser:', loggedInUser);
-
-
-//   const handleSubmit = (e: FormEvent) => {
-//     e.preventDefault();
-
-//     if (!loggedInUser) {
-//       setShowErrorModal(true);
-//       return;
-//     }
-
-//     onSavePostClicked(); 
-//   };
-
-
-//   const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
-//   const isSaveButtonDisabled = !agent; 
-
- 
-
-//   return (
-//     <section className="p-4 bg-gray-900 text-white rounded shadow-lg">
-//   <h2 className="text-2xl font-bold mb-4">Share Your Gaming Experience</h2>
-//   {loggedInUser === null && (
-//       <ErrorModal onClose={() => setShowErrorModal(false)} message={''} />
-//     )}
-
-//   <form onSubmit={handleSubmit}>
-//   <div className="mb-4">
-//           <label htmlFor="agentSelect" className="block font-semibold mb-2 text-xl">
-//             Select Agent:
-//           </label>
-//           <select
-//             id="agentSelect"
-//             name="agentSelect"
-//             value={agent}
-//             onChange={onAgentChanged}
-//             className="border border-gray-800 rounded p-2 w-full bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
-//           >
-//             <option value="">Select an agent</option>
-//             <option value="gekko">Gekko</option>
-//             <option value="fade">Fade</option>
-//           </select>
-//         </div>
-
-//         <div className="mb-4">
-//           <label htmlFor="postAuthor" className="block font-semibold mb-2 text-xl">
-//             Select Author:
-//           </label>
-//           <select
-//             id="postAuthor"
-//             value={userId}
-//             onChange={onAuthorChanged}
-//             className="border border-gray-800 rounded p-2 w-full bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
-//           >
-//             <option value="">Select an Author (placeholder)</option>
-//             <option value={loggedInUser?.id}>{loggedInUser?.name}</option>
-
-//           </select>
-//         </div>
-
-//         <label htmlFor="postTitle" className="block font-semibold mb-2 text-xl">
-//           Game Title:
-//         </label>
-//         <input
-//           type="text"
-//           id="postTitle"
-//           name="postTitle"
-//           value={title}
-//           onChange={onTitleChanged}
-//           className="border border-gray-800 rounded p-2 mb-4 w-full bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
-//           placeholder="Enter the game title"
-//         />
-
-//         <label htmlFor="postContent" className="block font-semibold mb-2 text-xl">
-//           Walkthrough Details:
-//         </label>
-//         <textarea
-//           id="postContent"
-//           name="postContent"
-//           value={content}
-//           onChange={onContentChanged}
-//           className="border border-gray-800 rounded p-4 mb-4 w-full h-48 bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
-//           placeholder="Share your in-depth walkthrough here..."
-//         />
-
-//         <label htmlFor="postVideo" className="block font-semibold mb-2 text-xl">
-//           Video URL:
-//         </label>
-//         <input
-//           type="text"
-//           id="postVideo"
-//           name="postVideo"
-//           value={videoUrl}
-//           onChange={onVideoUrlChanged}
-//           className="border border-gray-800 rounded p-2 mb-4 w-full bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
-//           placeholder="Paste video URL here"
-//         />
-
-//         <button
-//           type="submit"
-//           disabled={isSaveButtonDisabled && !canSave} 
-//           className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-//         >
-//           Save Post
-//         </button>
-//       </form>
-//       {showSuccessMessage && (
-//         <p className="text-green-500 mt-2">
-//           Post submitted for approval!
-//         </p>
-//       )}
-//     </section>
-//   );
-//   }
-
--- val-app/src/features/posts/PostsList.tsx
+++ val-app/src/features/posts/PostsList.tsx
@@ -1,68 +1,79 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import YouTube from 'react-youtube';
-// import {PostAuthor} from './PostAuthor'
-// import { TimeAgo } from './TimeAgo';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import YouTube from 'react-youtube';
+import { db, auth } from '../../firebase/firebase-config'; // Import the Firestore and Auth instances
+import { PostAuthor } from './PostAuthor';
+import { TimeAgo } from './TimeAgo';
 
-// export interface Post {
-//   id: string;
-//   date: string;
-//   title: string;
-//   content: string;
-//   videoUrl: string;
-//   agent: string;
-//   userId: string
-// }
+interface Post {
+  id: string;
+  date: string;
+  title: string;
+  content: string;
+  videoUrl: string;
+  agent: string;
+  userId: string;
+}
 
-// interface Props {
-//   posts: Post[];
-// }
+interface Props {
+  posts: Post[];
+}
 
+export const PostsList: React.FC<Props> = ({ posts }) => {
+  const [user, setUser] = useState<any>(null); // Use 'user' instead of 'loggedInUser'
 
+  useEffect(() => {
+    // Set the user using Firebase Auth
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
 
-// export const PostsList: React.FC<Props> = ({ posts }) => {
-//   const extractVideoId = (url: string): string | undefined => {
-//     const match = url.match(
-//       /(?:youtu\.be\/|youtube(?:-nocookie)?\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))([^&?]+)/
-//     );
-//     return match?.[1];
-//   };
+    return () => {
+      unsubscribe(); // Cleanup the auth state listener
+    };
+  }, []);
 
+  const extractVideoId = (url: string): string | undefined => {
+    const match = url.match(
+      /(?:youtu\.be\/|youtube(?:-nocookie)?\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))([^&?]+)/
+    );
+    return match?.[1];
+  };
 
-//   const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+  const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date));
 
-//   const renderedPosts = orderedPosts.map((post: Post) => {
-//     const videoId = extractVideoId(post.videoUrl);
+  const renderedPosts = orderedPosts.map((post: Post) => {
+    const videoId = extractVideoId(post.videoUrl);
 
-//     return (
-//       <article className="post-excerpt p-6 bg-gray-900 text-white rounded shadow-lg" key={post.id}>
-//         <h2 className="text-3xl font-bold mb-4">{post.title}</h2>
-//         <PostAuthor userId={post.userId} />
-//         <TimeAgo timestamp={post.date}/>
-//         {videoId && (
-//           <div className="flex justify-center mb-6">
-//             <YouTube videoId={videoId} />
-//           </div>
-//         )}
-//         <p className="post-content">{post.content.substring(0, 100)}</p>
-//         <Link to={`/posts/${post.agent}/${post.id}`} className="button muted-button">
-//           View Post
-//         </Link>
-//         <Link to={`/editPost/${post.agent}/${post.id}`} className="button muted-button">
-//           Edit Post
-//         </Link>
-//       </article>
-//     );
-//   });
+    return (
+      <article className="post-excerpt p-6 bg-gray-900 text-white rounded shadow-lg" key={post.id}>
+        <h2 className="text-3xl font-bold mb-4">{post.title}</h2>
+        <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
+        {videoId && (
+          <div className="flex justify-center mb-6">
+            <YouTube videoId={videoId} />
+          </div>
+        )}
+        <p className="post-content">{post.content.substring(0, 100)}</p>
+        <Link to={`/posts/${post.agent}/${post.id}`} className="button muted-button">
+          View Post
+        </Link>
+        <Link to={`/editPost/${post.agent}/${post.id}`} className="button muted-button">
+          Edit Post
+        </Link>
+      </article>
+    );
+  });
 
-//   return (
-//     <section className="container mx-auto bg-gray-900 py-10">
-//       <h1 className="text-4xl font-bold mb-8 text-white text-center tracking-wider">
-//         Community Shared Articles
-//       </h1>
-//       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center">
-//         {renderedPosts}
-//       </div>
-//     </section>
-//   );
-// };
+  return (
+    <section className="container mx-auto bg-gray-900 py-10">
+      <h1 className="text-4xl font-bold mb-8 text-white text-center tracking-wider">
+        Community Shared Articles
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center">
+        {renderedPosts}
+      </div>
+    </section>
+  );
+};
