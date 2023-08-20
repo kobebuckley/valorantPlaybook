491aef940578262152b27370a1f097c0775ab7ba
Kobe <kobetbuckley@gmail.com>
2023-08-19 14:57:58 -1000

    adding similar update logic from edit post page to ensure working, in progress

--- val-app/src/features/posts/ReactButton.tsx
+++ val-app/src/features/posts/ReactButton.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { reactionAdded } from './postsSlice';
 import { Post, fetchPosts, postUpdated, selectPostById } from './postsSlice';
+import { collection, getDocs } from '@firebase/firestore';
+import { db } from '../../firebase/firebase-config';
+import { AppDispatch } from '../../app/store';
+import { useParams } from 'react-router-dom';
 
 const reactionEmoji = {
   thumbsUp: '👍',
@@ -10,15 +14,70 @@ const reactionEmoji = {
   rocket: '🚀',
   eyes: '👀',
 };
-
 interface ReactionButtonsProps {
   post: Post;
 }
+export const ReactionButtons: React.FC<ReactionButtonsProps> = ({ post }) => {
+  const [selectedDocData, setSelectedDocData] = useState<any | null>(null);
+  const dispatch = useDispatch<AppDispatch>();
+  const { id } = useParams<{ id: string }>(); // Only need the ID parameter here
 
+console.log("Selected post:", post);
+
+
+useEffect(() => {
+  const fetchData = async () => {
+    // if (!post && id) {
+    //   try {
+    //     await dispatch(fetchPosts());
+    //   } catch (error) {
+    //     console.error('Error fetching posts:', error);
+    //   }
+    // }
+
+    // if (post) {
+    //   setTitle(post.title);
+    //   setContent(post.content);
+    //   setVideoUrl(post.videoUrl);
+    //   setAgent(post.agent);
+    // }
+
+    try {
+      const querySnapshot = await getDocs(collection(db, 'posts'));
+      let foundSelectedDocData = null;
+
+      querySnapshot.docs.forEach(doc => {
+        const postRef = doc
+        const postData = doc.data();
+        if (postData.id == post?.id) {
+          foundSelectedDocData = postRef;
+
+        }
+        // console.log("Post Id:", postData.id);
+      });
+
+      setSelectedDocData(foundSelectedDocData);
+      if (foundSelectedDocData) {
+        console.log("Selected Document Data:", foundSelectedDocData);
+        return
+      } else {
+        console.log("No document with matching ID found.");
+      }
+    } catch (error) {
+      console.error("Error fetching and logging collection:", error);
+    }
+  };
+
+  fetchData();
+}, [dispatch, id, post]);
+
+
+
+// console.log('TEST HERE',selectedDocData.id)
+  console.log('The Doc that will be updated',selectedDocData)
+  
 
 // update me as well
-export const ReactionButtons: React.FC<ReactionButtonsProps> = ({ post }) => {
-  const dispatch = useDispatch();
 
   const reactionButtons = Object.entries(reactionEmoji).map(([name, emoji]) => {
     return (
