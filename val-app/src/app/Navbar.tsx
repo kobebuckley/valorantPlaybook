e53c137486377123284768462f6fb8139950a009
Kobe <kobetbuckley@gmail.com>
2023-08-22 21:44:09 -1000

    in progress fixing nav styling

--- val-app/src/app/Navbar.tsx
+++ val-app/src/app/Navbar.tsx
@@ -29,55 +29,54 @@ export const Navbar: React.FC<NavbarProps> = ({ title }) => {
   console.log("display name is:" + user?.displayName)
 
   return (
-    <nav className="bg-indigo-600 py-4 text-white sticky top-0 z-50">
-      <section className="container mx-auto flex justify-between items-center">
-        <div>
+    <nav className="bg-indigo-700 py-4 text-white sticky top-0 z-50">
+      <section className="container mx-auto flex flex-col sm:flex-row justify-between items-center">
+        <div className="mb-4 sm:mb-0">
           <Link
             to="/"
-            className={`text-3xl font-bold tracking-tight transition-colors duration-300 hover:text-yellow-400`}
+            className={`text-4xl font-bold tracking-tight transition-colors duration-300 hover:text-yellow-400`}
           >
             {title}
           </Link>
         </div>
 
-        <div>
+        <div className="mb-4 sm:mb-0">
           {user ? (
-            <div>
-              <span className="text-white mr-4">Hello, {user.displayName}!</span>
+            <div className="mb-2 sm:mb-0">
+              <span className="text-white mr-2 text-lg">Hello, {user.displayName}!</span>
               <button
                 onClick={handleSignOut}
-                className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-700 focus:outline-none focus:ring focus:ring-red-300"
+                className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-700 focus:outline-none focus:ring focus:ring-red-300 text-lg"
               >
                 Sign Out
               </button>
             </div>
           ) : (
-            <Link to="/login" className="text-white hover:text-gray-300 font-bold text-xl">
+            <Link
+              to="/login"
+              className="text-white hover:text-gray-300 font-bold text-lg"
+            >
               Login
             </Link>
           )}
         </div>
 
-        <div className="navContent ml-4 flex items-center space-x-4">
+        <div className="navContent mb-4 sm:mb-0 flex items-center space-x-2 sm:space-x-4">
           {!user && (
-            <div>
-              <Link
-                to="/register"
-                className={`text-xl font-bold tracking-tight transition-colors duration-300 hover:text-yellow-400`}
-              >
-                Register
-              </Link>
-            </div>
+            <Link
+              to="/register"
+              className="text-lg font-bold tracking-tight transition-colors duration-300 hover:text-yellow-400"
+            >
+              Register
+            </Link>
           )}
 
-          <div>
-            <Link
-              to="/addPost"
-              className={`text-xl font-bold tracking-tight transition-colors duration-300 hover:text-yellow-400`}
-            >
-              Add Post
-            </Link>
-          </div>
+          <Link
+            to="/addPost"
+            className="text-lg font-bold tracking-tight transition-colors duration-300 hover:text-yellow-400"
+          >
+            Add Post
+          </Link>
         </div>
       </section>
     </nav>
